f76c57e73ffa4ffa3e34f4d027e575e4f4969cc0
Gus Figue <Gustavo.pereira1204@hotmail.com>
2025-10-03 21:28:56 -0300

    Add files via upload

---
+++ CRUD_spring/src/main/java/com/example/CRUD_spring/Controller/UsuarioController.java
@@ -0,0 +1,33 @@
+package com.example.CRUD_spring.Controller;
+
+
+import com.example.CRUD_spring.Business.UsuarioService;
+import com.example.CRUD_spring.Infrastructure.entitys.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario) {
+        usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+        usuarioService.deletarUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+    }
+}
--- CRUD_spring/src/main/java/com/example/CRUD_spring/Infrastructure/repository/UsuarioRepository.java
+++ CRUD_spring/src/main/java/com/example/CRUD_spring/Infrastructure/repository/UsuarioRepository.java
@@ -2,11 +2,14 @@ package com.example.CRUD_spring.Infrastructure.repository;
 
 import com.example.CRUD_spring.Infrastructure.entitys.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> { //JpaRepository<*NomedaTabela*, *TipodoID*>
+import java.util.Optional;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+    Optional<Usuario> findByEmail(String email);
 
-
-
+    @Transactional
+    void deleteByEmail(String email);
 }
